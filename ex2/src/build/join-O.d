ce598a22b4c6cbad48d9419fe9b1e9f8732e9265
sch4umbu <75167758+sch4umbu@users.noreply.github.com>
2021-01-15 11:52:41 +0100

    Delete ex2/src/build directory

--- ex2/src/build/join-O.d
+++
@@ -1,2 +0,0 @@
-build/./join-O.o: join.cpp join.hpp graph.hpp utils.hpp \
- ../blossom5-v2.05.src/PerfectMatching.h ../blossom5-v2.05.src/block.h
--- ex2/src/build/join-g.d
+++
@@ -1,2 +0,0 @@
-build/./join-g.o: join.cpp join.hpp graph.hpp utils.hpp \
- ../blossom5-v2.05.src/PerfectMatching.h ../blossom5-v2.05.src/block.h
